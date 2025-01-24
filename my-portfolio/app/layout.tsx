40be73ee9f8961595890a6629a0ee334db086cfc
Austin Perez <austin.perez760@gmail.com>
2025-01-23 21:44:10 -0600

    Created header with animations from framer-motion

--- my-portfolio/app/layout.tsx
+++ my-portfolio/app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css';
 import { Inter } from 'next/font/google';
+import Header from '@/components/header';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -16,9 +17,9 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={`${inter.className} bg-slate-50 text-slate-950`}>
-        <div className='bg-slate-400 absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]'></div>
+        <div className='bg-sky-800 absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]'></div>
         <div className='bg-stone-400 absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]'></div>
-
+        <Header />
         {children}
       </body>
     </html>
