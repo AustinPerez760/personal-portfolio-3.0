46b721976474d0891ccc5f986156e0dc8461e145
Austin Perez <austin.perez760@gmail.com>
2025-01-23 22:04:11 -0600

    Fixed background color not applying  when on mobile devices

--- my-portfolio/app/layout.tsx
+++ my-portfolio/app/layout.tsx
@@ -11,14 +11,14 @@ export const metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang='en'>
       <body className={`${inter.className} bg-slate-50 text-slate-950`}>
-        <div className='bg-sky-800 absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]'></div>
-        <div className='bg-stone-400 absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]'></div>
+        <div className='bg-sky-800 absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]'></div>
+        <div className='bg-red-400 absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]'></div>
         <Header />
         {children}
       </body>
