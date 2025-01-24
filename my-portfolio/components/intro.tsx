3c999a76eb35a35f69159d743dcc4c78a2ed2011
Austin Perez <austin.perez760@gmail.com>
2025-01-24 12:38:03 -0600

    Implmented animations for portfolio image and emoji

--- my-portfolio/components/intro.tsx
+++ my-portfolio/components/intro.tsx
@@ -1,20 +1,34 @@
+'use client';
+
 import Image from 'next/image';
 import React from 'react';
+import { motion } from 'framer-motion';
 
 export default function Intro() {
   return (
     <section>
       <div className='flex items-center justify-center'>
-        <div>
-          <Image
-            src='/images/portfolio-pic.jpeg'
-            alt='Sam portrait'
-            width='192'
-            height='192'
-            quality='95'
-            priority={true}
-            className='h-34 w-34 rounded-full border-[0.35rem] border-white shadow-xl'></Image>
-          <span className='text-4xl'>🦦</span>
+        <div className='relative'>
+          <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ type: 'tween', duration: 0.2 }}>
+            <Image
+              src='/images/portfolio-pic.jpeg'
+              alt='Sam portrait'
+              width='192'
+              height='192'
+              quality='95'
+              priority={true}
+              className='h-34 w-34 rounded-full border-[0.35rem] border-white shadow-xl'></Image>
+          </motion.div>
+          <motion.span
+            className='absolute bottom-0 right-0  text-4xl'
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ type: 'tween', delay: 0.3, duration: 0.3 }}>
+            🏆
+          </motion.span>
         </div>
       </div>
     </section>
